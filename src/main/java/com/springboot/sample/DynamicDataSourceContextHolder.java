b1dc07b102a5a2fb7ffbf6155b13e1470778c23a
jack-huang2016 <2456120975@qq.com>
2022-11-10 10:37:26 +0800

    Update DynamicDataSourceContextHolder.java

--- src/main/java/com/springboot/sample/DynamicDataSourceContextHolder.java
+++ src/main/java/com/springboot/sample/DynamicDataSourceContextHolder.java
@@ -3,18 +3,34 @@ package com.springboot.sample;
 import java.util.ArrayList;
 import java.util.List;
 
+/**
+ * 数据源操作类
+ */
 public class DynamicDataSourceContextHolder {
-	private static final ThreadLocal<String> contextHolder = new ThreadLocal<String>();
-    public static List<String> dataSourceIds = new ArrayList<>(); //存放所有数据源
+    //存放当前线程使用的数据源类型信息
+    private static final ThreadLocal<String> contextHolder = new ThreadLocal<String>();
 
+    //存放所有数据源
+    public static List<String> dataSourceIds = new ArrayList<>(); 
+
+    /**
+     * 绑定当前线程数据源路由的key
+     * 使用完成后必须调用clearDataSourceType()方法删除
+     */
     public static void setDataSourceType(String dataSourceType) {
         contextHolder.set(dataSourceType);
     }
 
+   /**
+    * 获取当前线程的数据源路由的key
+    */
     public static String getDataSourceType() {
         return contextHolder.get();
     }
 
+   /**
+    * 删除与当前线程绑定的数据源路由的key
+    */
     public static void clearDataSourceType() {
         contextHolder.remove();
     }
